e6cec0ebfc5fc48ba6aef12ff0cc7c99d48de7e3
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2022-01-10 20:21:33 +0530

    Added: if elif else block

---
+++ IfElifElse/ifElifElse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -x
+num1=100;
+num2=100;
+if [ $num1 -gt  $num2 ]
+then
+        echo "The numbers are greater than 100";
+
+elif [ $num1 -eq  $num2 ]
+then
+         echo "numbers are equal";
+else
+	echo "the number is less";
+fi 
+
+
+
+
