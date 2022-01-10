dab8006433cfdf86b31ca24828ff51153d5d2195
ShrutiBelsare27 <shrutibelsare27@gmail.com>
2022-01-10 19:02:38 +0530

    Add:Sum of two number

---
+++ add.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x 
+num1=30;
+num2=10;
+add=$(( $num1+ $num2));
+echo $add;
+mul=$(($num1*$num2));
+echo $mul;
+sub= $(( $num1 - $num2 ));
+echo $sub;
+div=$(($num1 / $num2));
+echo $div;
+
